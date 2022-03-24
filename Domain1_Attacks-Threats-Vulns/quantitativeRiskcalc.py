a27afde599f3cd08d5983d497bdcdcdf39f6cd05
mrcodelab <96273662+mrcodelab@users.noreply.github.com>
2022-03-24 13:10:06 -0500

    Day4 afternoon break
    updated risk calculator, updates to glossary.json, etc

--- Domain1_Attacks-Threats-Vulns/quantitativeRiskcalc.py
+++ Domain1_Attacks-Threats-Vulns/quantitativeRiskcalc.py
@@ -5,12 +5,14 @@ Annual Rate of Occurance (aro): calculation of number of times a given risk
 Annual Loss Expectancy (ale): total amount of loss in a year due to a specific
     event. ALE = SLE * ARO
 """
+asset_name = str(input("What asset are you assessing?\n"))
 risk_event = str(input("What event is this estimate addressing?\n"))
 asset_value = float(input("What is asset value \n"))
 aro = float(input("How many times do you think this will happen this year?\n"))
 
 
 def ale():
+    print("The SLE for {0} is {1}.".format(asset_name, asset_value))
     ale_amount = asset_value * aro
     print("The ALE for {0}(s) is ${1}.".format(risk_event, ale_amount))
     return(ale_amount)
